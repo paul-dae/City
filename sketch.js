67e2a703ba55271a737ca1e4ef70cbdd44e00df5
paul-dae <paul.daechert@hotmail.com>
2018-08-24 10:38:12 +0200

    added car.js script and some testing

--- sketch.js
+++ sketch.js
@@ -1,7 +1,13 @@
+var c;
+var scaling = 0.1;
+
 function setup() {
-	createCanvas(windowWidth, windowHeight);
+	createCanvas(800, 800);
+    c = new Car();
+    c.createRandom();
 }
 
 function draw() {
-
+    background(51);
+    c.update();
 }
